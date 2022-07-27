8184ab0a4a41dfae95b99b549e97ccb4d32eceec
Steven Normore <snormore@gmail.com>
2022-07-27 15:11:00 -0400

    Add e2e runner (#66)
    * Add e2e runner docker image and update test to support continuous
    running
    
    * Fix e2e test func reference
    
    * Fix e2e dockerfile entrypoint reference
    
    * Fix e2e docker build
    
    * Fix e2e deploy workflow action

--- pkg/e2e/e2e_test.go
+++ pkg/e2e/e2e_test.go
@@ -5,13 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/status-im/go-waku/waku/v2/node"
 	wakunode "github.com/status-im/go-waku/waku/v2/node"
 	wakuprotocol "github.com/status-im/go-waku/waku/v2/protocol"
 	"github.com/status-im/go-waku/waku/v2/protocol/pb"
@@ -23,67 +20,105 @@ import (
 )
 
 var (
-	envShouldRunE2ETests = envVarBool("E2E")
-	envNetworkEnv        = envVar("XMTP_E2E_ENV", "dev")
-	envBootstrapAddrs    = envVarStrings("XMTP_E2E_BOOTSTRAP_ADDRS")
-	envNodesURL          = envVar("XMTP_E2E_NODES_URL", "https://nodes.xmtp.com")
+	envShouldRunE2ETests             = envVarBool("E2E")
+	envShouldRunE2ETestsContinuously = envVarBool("E2E_CONTINUOUS")
+	envNetworkEnv                    = envVar("XMTPD_E2E_ENV", "dev")
+	envBootstrapAddrs                = envVarStrings("XMTPD_E2E_BOOTSTRAP_ADDRS")
+	envNodesURL                      = envVar("XMTPD_E2E_NODES_URL", "https://nodes.xmtp.com")
+	envDelayBetweenRunsSeconds       = envVarInt("XMTPD_E2E_DELAY", 5)
 )
 
 func TestE2E(t *testing.T) {
-	t.Run("publish subscribe query across nodes", func(t *testing.T) {
-		if !envShouldRunE2ETests {
-			t.SkipNow()
-		}
+	if !envShouldRunE2ETests && !envShouldRunE2ETestsContinuously {
+		t.Skip("E2E env not set")
+	}
+	ctx := context.Background()
+	withMetricsServer(t, ctx, func(t *testing.T) {
+		for {
+			runTest(t, ctx, "publish subscribe query", testPublishSubscribeQuery)
 
-		// Fetch bootstrap node addresses.
-		var bootstrapAddrs []string
-		if len(envBootstrapAddrs) == 0 {
-			var err error
-			bootstrapAddrs, err = fetchBootstrapAddrs(envNetworkEnv)
-			require.NoError(t, err)
-			require.NotEmpty(t, bootstrapAddrs)
-			require.Len(t, bootstrapAddrs, 3)
-		} else {
-			bootstrapAddrs = envBootstrapAddrs
-		}
-
-		// Create a client node for each bootstrap node, and connect to it.
-		clients := make([]*wakunode.WakuNode, len(bootstrapAddrs))
-		for i, addr := range bootstrapAddrs {
-			c, cleanup := test.NewNode(t, nil)
-			defer cleanup()
-			test.ConnectWithAddr(t, c, addr)
-			clients[i] = c
-		}
-		time.Sleep(500 * time.Millisecond)
-
-		// Subscribe to a topic on each client, connected to each node.
-		contentTopic := "test-" + test.RandomStringLower(5)
-		envCs := make([]chan *wakuprotocol.Envelope, len(clients))
-		for i, c := range clients {
-			envCs[i] = test.SubscribeTo(t, c, []string{contentTopic})
-		}
-		time.Sleep(500 * time.Millisecond)
-
-		// Send a message to every node.
-		msgs := make([]*pb.WakuMessage, len(clients))
-		for i := range clients {
-			msgs[i] = test.NewMessage(contentTopic, int64(i+1), fmt.Sprintf("msg%d", i+1))
-		}
-		for i, sender := range clients {
-			test.Publish(t, sender, msgs[i])
-		}
-
-		// Expect them to be relayed to all nodes.
-		for _, envC := range envCs {
-			test.SubscribeExpect(t, envC, msgs)
-		}
-
-		// Expect that they've all been stored on each node.
-		for i, c := range clients {
-			expectQueryMessagesEventually(t, c, bootstrapAddrs[i], []string{contentTopic}, msgs)
+			if !envShouldRunE2ETestsContinuously {
+				break
+			}
+			time.Sleep(time.Duration(envDelayBetweenRunsSeconds) * time.Second)
 		}
 	})
+}
+
+func runTest(t *testing.T, ctx context.Context, name string, fn func(t *testing.T)) {
+	t.Run(testName(name), func(t *testing.T) {
+		nameTag := newTag(testNameTagKey, name)
+		started := time.Now().UTC()
+		defer func() {
+			ended := time.Now().UTC()
+			err := recordRunDuration(ctx, ended.Sub(started), nameTag)
+			require.NoError(t, err)
+			if t.Failed() {
+				err := recordFailedRun(ctx, nameTag)
+				require.NoError(t, err)
+			} else {
+				err := recordSuccessfulRun(ctx, nameTag)
+				require.NoError(t, err)
+			}
+		}()
+
+		fn(t)
+	})
+}
+
+func testPublishSubscribeQuery(t *testing.T) {
+	// Fetch bootstrap node addresses.
+	var bootstrapAddrs []string
+	if len(envBootstrapAddrs) == 0 {
+		var err error
+		bootstrapAddrs, err = fetchBootstrapAddrs(envNetworkEnv)
+		require.NoError(t, err)
+		require.NotEmpty(t, bootstrapAddrs)
+		require.Len(t, bootstrapAddrs, 3)
+	} else {
+		bootstrapAddrs = envBootstrapAddrs
+	}
+
+	// Create a client node for each bootstrap node, and connect to it.
+	clients := make([]*wakunode.WakuNode, len(bootstrapAddrs))
+	for i, addr := range bootstrapAddrs {
+		c, cleanup := test.NewNode(t, nil)
+		defer cleanup()
+		test.ConnectWithAddr(t, c, addr)
+		clients[i] = c
+	}
+	time.Sleep(500 * time.Millisecond)
+
+	// Subscribe to a topic on each client, connected to each node.
+	contentTopic := "test-" + test.RandomStringLower(5)
+	envCs := make([]chan *wakuprotocol.Envelope, len(clients))
+	for i, c := range clients {
+		envCs[i] = test.SubscribeTo(t, c, []string{contentTopic})
+	}
+	time.Sleep(500 * time.Millisecond)
+
+	// Send a message to every node.
+	msgs := make([]*pb.WakuMessage, len(clients))
+	for i := range clients {
+		msgs[i] = test.NewMessage(contentTopic, int64(i+1), fmt.Sprintf("msg%d", i+1))
+	}
+	for i, sender := range clients {
+		test.Publish(t, sender, msgs[i])
+	}
+
+	// Expect them to be relayed to all nodes.
+	for _, envC := range envCs {
+		test.SubscribeExpect(t, envC, msgs)
+	}
+
+	// Expect that they've all been stored on each node.
+	for i, c := range clients {
+		expectQueryMessagesEventually(t, c, bootstrapAddrs[i], []string{contentTopic}, msgs)
+	}
+}
+
+func testName(s string) string {
+	return fmt.Sprintf("%s/%d", s, time.Now().UTC().Unix())
 }
 
 func fetchBootstrapAddrs(env string) ([]string, error) {
@@ -111,33 +146,7 @@ func fetchBootstrapAddrs(env string) ([]string, error) {
 	return addrs, nil
 }
 
-func envVar(name, defaultVal string) string {
-	val := os.Getenv(name)
-	if val == "" {
-		return defaultVal
-	}
-	return val
-}
-
-func envVarStrings(name string) []string {
-	val := os.Getenv(name)
-	vals := strings.Split(val, ",")
-	retVals := make([]string, 0, len(vals))
-	for _, v := range vals {
-		if v == "" {
-			continue
-		}
-		retVals = append(retVals, v)
-	}
-	return retVals
-}
-
-func envVarBool(name string) bool {
-	valStr := os.Getenv(name)
-	return valStr != ""
-}
-
-func queryMessages(t *testing.T, c *node.WakuNode, peerAddr string, contentTopics []string) []*pb.WakuMessage {
+func queryMessages(t *testing.T, c *wakunode.WakuNode, peerAddr string, contentTopics []string) []*pb.WakuMessage {
 	log, err := zap.NewDevelopment()
 	require.NoError(t, err)
 
@@ -172,7 +181,7 @@ func queryMessages(t *testing.T, c *node.WakuNode, peerAddr string, contentTopic
 	return msgs
 }
 
-func expectQueryMessagesEventually(t *testing.T, n *node.WakuNode, peerAddr string, contentTopics []string, expectedMsgs []*pb.WakuMessage) []*pb.WakuMessage {
+func expectQueryMessagesEventually(t *testing.T, n *wakunode.WakuNode, peerAddr string, contentTopics []string, expectedMsgs []*pb.WakuMessage) []*pb.WakuMessage {
 	var msgs []*pb.WakuMessage
 	require.Eventually(t, func() bool {
 		msgs = queryMessages(t, n, peerAddr, contentTopics)
