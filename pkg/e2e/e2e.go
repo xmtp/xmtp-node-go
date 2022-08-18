d67a69745eedb5a1f530b671fd05df179b4090a1
Steven Normore <snormore@gmail.com>
2022-08-18 15:17:24 -0400

    Refactor e2e test to avoid failure on sigterm

---
+++ pkg/e2e/e2e.go
@@ -0,0 +1,328 @@
+package e2e
+
+import (
+	"bytes"
+	"context"
+	"crypto/ecdsa"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/rand"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/status-im/go-waku/tests"
+	wakunode "github.com/status-im/go-waku/waku/v2/node"
+	wakuprotocol "github.com/status-im/go-waku/waku/v2/protocol"
+	wakufilter "github.com/status-im/go-waku/waku/v2/protocol/filter"
+	wakupb "github.com/status-im/go-waku/waku/v2/protocol/pb"
+	wakurelay "github.com/status-im/go-waku/waku/v2/protocol/relay"
+	"github.com/xmtp/xmtp-node-go/pkg/store"
+	"go.uber.org/zap"
+
+	_ "net/http/pprof"
+)
+
+type E2E struct {
+	ctx context.Context
+	log *zap.Logger
+
+	config *Config
+}
+
+type Config struct {
+	ShouldRunContinuously   bool
+	NetworkEnv              string
+	BootstrapAddrs          []string
+	NodesURL                string
+	DelayBetweenRunsSeconds int
+}
+
+func New(ctx context.Context, log *zap.Logger, config *Config) *E2E {
+	e := &E2E{
+		ctx:    ctx,
+		log:    log,
+		config: config,
+	}
+	return e
+}
+
+func (e *E2E) Run() error {
+	if e.config.ShouldRunContinuously {
+		go func() {
+			err := http.ListenAndServe("localhost:6060", nil)
+			if err != nil {
+				e.log.Error("serving profiler", zap.Error(err))
+			}
+		}()
+	}
+
+	err := e.withMetricsServer(func() {
+		for {
+			err := e.runTest("publish subscribe query", e.testPublishSubscribeQuery)
+			if err != nil {
+				e.log.Error("test failed", zap.Error(err))
+			} else {
+				e.log.Info("test passed")
+			}
+
+			if !e.config.ShouldRunContinuously {
+				break
+			}
+			time.Sleep(time.Duration(e.config.DelayBetweenRunsSeconds) * time.Second)
+		}
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *E2E) runTest(name string, fn func() error) error {
+	nameTag := newTag(testNameTagKey, name)
+	started := time.Now().UTC()
+
+	err := fn()
+	if err != nil {
+		recordErr := recordFailedRun(e.ctx, nameTag)
+		if recordErr != nil {
+			e.log.Error("recording failed run metric", zap.Error(err))
+		}
+		return err
+	}
+	ended := time.Now().UTC()
+
+	err = recordSuccessfulRun(e.ctx, nameTag)
+	if err != nil {
+		return err
+	}
+
+	err = recordRunDuration(e.ctx, ended.Sub(started), nameTag)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fetchBootstrapAddrs(nodesURL string, env string) ([]string, error) {
+	client := &http.Client{}
+	r, err := client.Get(nodesURL)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	var manifest map[string]interface{}
+	err = json.NewDecoder(r.Body).Decode(&manifest)
+	if err != nil {
+		return nil, err
+	}
+
+	envManifest := manifest[env].(map[string]interface{})
+	addrs := make([]string, len(envManifest))
+	i := 0
+	for _, addr := range envManifest {
+		addrs[i] = addr.(string)
+		i++
+	}
+
+	return addrs, nil
+}
+
+func newNode(log *zap.Logger, opts ...wakunode.WakuNodeOption) (*wakunode.WakuNode, func(), error) {
+	hostAddr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:0")
+
+	prvKey, err := newPrivateKey()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ctx := context.Background()
+	opts = append([]wakunode.WakuNodeOption{
+		wakunode.WithLogger(log),
+		wakunode.WithPrivateKey(prvKey),
+		wakunode.WithHostAddress(hostAddr),
+		wakunode.WithWakuRelay(),
+		wakunode.WithWakuFilter(true),
+		wakunode.WithWebsockets("0.0.0.0", 0),
+	}, opts...)
+	node, err := wakunode.New(ctx, opts...)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = node.Start()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return node, func() {
+		node.Stop()
+	}, nil
+}
+
+func newPrivateKey() (*ecdsa.PrivateKey, error) {
+	key, err := tests.RandomHex(32)
+	if err != nil {
+		return nil, err
+	}
+
+	prvKey, err := crypto.HexToECDSA(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return prvKey, nil
+}
+
+func connectWithAddr(ctx context.Context, n *wakunode.WakuNode, addr string) error {
+	err := n.DialPeer(ctx, addr)
+	if err != nil {
+		return err
+	}
+
+	// This delay is necessary, but it's unclear why at this point. We see
+	// similar delays throughout the waku codebase as well for this reason.
+	time.Sleep(100 * time.Millisecond)
+
+	return nil
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randomString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func randomStringLower(n int) string {
+	return strings.ToLower(randomString(n))
+}
+
+func subscribeTo(ctx context.Context, n *wakunode.WakuNode, contentTopics []string) (chan *wakuprotocol.Envelope, error) {
+	_, f, err := n.Filter().Subscribe(ctx, wakufilter.ContentFilter{
+		ContentTopics: contentTopics,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return f.Chan, nil
+}
+
+func newMessage(contentTopic string, timestamp int64, content string) *wakupb.WakuMessage {
+	return &wakupb.WakuMessage{
+		Payload:      []byte(content),
+		ContentTopic: contentTopic,
+		Timestamp:    timestamp,
+	}
+}
+
+func publish(ctx context.Context, n *wakunode.WakuNode, msg *wakupb.WakuMessage) error {
+	_, err := n.Relay().Publish(ctx, msg)
+	return err
+}
+
+func subscribeExpect(envC chan *wakuprotocol.Envelope, msgs []*wakupb.WakuMessage) error {
+	receivedMsgs := []*wakupb.WakuMessage{}
+	var done bool
+	for !done {
+		select {
+		case env := <-envC:
+			receivedMsgs = append(receivedMsgs, env.Message())
+			if len(receivedMsgs) == len(msgs) {
+				done = true
+			}
+		case <-time.After(5 * time.Second):
+			done = true
+		}
+	}
+	diff := cmp.Diff(msgs, receivedMsgs, cmpopts.SortSlices(wakuMessagesLessFn))
+	if diff != "" {
+		fmt.Println(diff)
+		return fmt.Errorf("expected equal, diff: %s", diff)
+	}
+	return nil
+}
+
+func expectQueryMessagesEventually(log *zap.Logger, n *wakunode.WakuNode, peerAddr string, contentTopics []string, expectedMsgs []*wakupb.WakuMessage) error {
+	timeout := 3 * time.Second
+	delay := 500 * time.Millisecond
+	started := time.Now()
+	for {
+		msgs, err := queryMessages(log, n, peerAddr, contentTopics)
+		if err != nil {
+			return err
+		}
+		if len(msgs) == len(expectedMsgs) {
+			diff := cmp.Diff(msgs, expectedMsgs, cmpopts.SortSlices(wakuMessagesLessFn))
+			if diff != "" {
+				fmt.Println(diff)
+				return fmt.Errorf("expected equal, diff: %s", diff)
+			}
+			break
+		}
+		if time.Since(started) > timeout {
+			return errors.New("timeout")
+		}
+		time.Sleep(delay)
+	}
+	return nil
+}
+
+func wakuMessagesLessFn(a, b *wakupb.WakuMessage) bool {
+	if a.ContentTopic != b.ContentTopic {
+		return a.ContentTopic < b.ContentTopic
+	}
+	if a.Timestamp != b.Timestamp {
+		return a.Timestamp < b.Timestamp
+	}
+	return bytes.Compare(a.Payload, b.Payload) < 0
+}
+
+func queryMessages(log *zap.Logger, c *wakunode.WakuNode, peerAddr string, contentTopics []string) ([]*wakupb.WakuMessage, error) {
+	pi, err := peer.AddrInfoFromString(peerAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := store.NewClient(
+		store.WithClientLog(log),
+		store.WithClientHost(c.Host()),
+		store.WithClientPeer(pi.ID),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	msgs := []*wakupb.WakuMessage{}
+	ctx := context.Background()
+	contentFilters := make([]*wakupb.ContentFilter, len(contentTopics))
+	for i, contentTopic := range contentTopics {
+		contentFilters[i] = &wakupb.ContentFilter{
+			ContentTopic: contentTopic,
+		}
+	}
+	_, err = client.Query(ctx, &wakupb.HistoryQuery{
+		PubsubTopic:    wakurelay.DefaultWakuTopic,
+		ContentFilters: contentFilters,
+	}, func(res *wakupb.HistoryResponse) (int, bool) {
+		msgs = append(msgs, res.Messages...)
+		return len(res.Messages), true
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return msgs, nil
+}
--- pkg/e2e/e2e_test.go
+++ pkg/e2e/e2e_test.go
@@ -2,23 +2,10 @@ package e2e
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"testing"
-	"time"
 
-	"github.com/libp2p/go-libp2p-core/peer"
-	wakunode "github.com/status-im/go-waku/waku/v2/node"
-	wakuprotocol "github.com/status-im/go-waku/waku/v2/protocol"
-	"github.com/status-im/go-waku/waku/v2/protocol/pb"
-	"github.com/status-im/go-waku/waku/v2/protocol/relay"
 	"github.com/stretchr/testify/require"
-	"github.com/xmtp/xmtp-node-go/pkg/store"
-	test "github.com/xmtp/xmtp-node-go/pkg/testing"
 	"go.uber.org/zap"
-
-	_ "net/http/pprof"
 )
 
 const (
@@ -26,203 +13,14 @@ const (
 	localNodesURL   = "http://localhost:8000"
 )
 
-var (
-	envShouldRunE2ETestsContinuously = envVarBool("E2E_CONTINUOUS")
-	envNetworkEnv                    = envVar("XMTPD_E2E_ENV", localNetworkEnv)
-	envBootstrapAddrs                = envVarStrings("XMTPD_E2E_BOOTSTRAP_ADDRS")
-	envNodesURL                      = envVar("XMTPD_E2E_NODES_URL", "https://nodes.xmtp.com")
-	envDelayBetweenRunsSeconds       = envVarInt("XMTPD_E2E_DELAY", 5)
-)
-
-func TestMain(m *testing.M) {
-	ctx := context.Background()
-	log, err := zap.NewDevelopment()
-	if err != nil {
-		panic(err)
-	}
-
-	if envShouldRunE2ETestsContinuously {
-		go func() {
-			err := http.ListenAndServe("localhost:6060", nil)
-			if err != nil {
-				log.Fatal("serving profiler", zap.Error(err))
-			}
-		}()
-	}
-	if envNetworkEnv == localNetworkEnv {
-		envNodesURL = localNodesURL
-	}
-
-	err = withMetricsServer(ctx, log, func() {
-		for {
-			exitCode := m.Run()
-			log.Info("test suite complete", zap.Int("exit_code", exitCode))
-
-			if !envShouldRunE2ETestsContinuously {
-				break
-			}
-			time.Sleep(time.Duration(envDelayBetweenRunsSeconds) * time.Second)
-		}
-	})
-	if err != nil {
-		log.Fatal("creating metrics server", zap.Error(err))
-	}
-}
-
 func TestE2E(t *testing.T) {
 	ctx := context.Background()
-	runTest(t, ctx, "publish subscribe query", testPublishSubscribeQuery)
-}
-
-func runTest(t *testing.T, ctx context.Context, name string, fn func(t *testing.T)) {
-	t.Run(testName(name), func(t *testing.T) {
-		nameTag := newTag(testNameTagKey, name)
-		started := time.Now().UTC()
-		defer func() {
-			ended := time.Now().UTC()
-			err := recordRunDuration(ctx, ended.Sub(started), nameTag)
-			require.NoError(t, err)
-			if t.Failed() {
-				err := recordFailedRun(ctx, nameTag)
-				require.NoError(t, err)
-			} else {
-				err := recordSuccessfulRun(ctx, nameTag)
-				require.NoError(t, err)
-			}
-		}()
-
-		fn(t)
-	})
-}
-
-func testPublishSubscribeQuery(t *testing.T) {
-	// Fetch bootstrap node addresses.
-	var bootstrapAddrs []string
-	if len(envBootstrapAddrs) == 0 {
-		var err error
-		bootstrapAddrs, err = fetchBootstrapAddrs(envNetworkEnv)
-		require.NoError(t, err)
-		require.NotEmpty(t, bootstrapAddrs)
-		require.Len(t, bootstrapAddrs, 3)
-	} else {
-		bootstrapAddrs = envBootstrapAddrs
-	}
-
-	// Create a client node for each bootstrap node, and connect to it.
-	clients := make([]*wakunode.WakuNode, len(bootstrapAddrs))
-	for i, addr := range bootstrapAddrs {
-		c, cleanup := test.NewNode(t, nil,
-			// Specify libp2p options here to avoid using the waku-default that
-			// enables the NAT service, which currently leaks goroutines over
-			// time when creating and destroying many in-process.
-			// https://github.com/libp2p/go-libp2p/blob/8de2efdb5cfb32daaec7fac71e977761b24be46d/config/config.go#L302
-			wakunode.WithLibP2POptions(),
-			wakunode.WithoutWakuRelay(),
-		)
-		defer cleanup()
-		test.ConnectWithAddr(t, c, addr)
-		clients[i] = c
-	}
-	time.Sleep(500 * time.Millisecond)
-
-	// Subscribe to a topic on each client, connected to each node.
-	contentTopic := "test-" + test.RandomStringLower(5)
-	envCs := make([]chan *wakuprotocol.Envelope, len(clients))
-	for i, c := range clients {
-		envCs[i] = test.SubscribeTo(t, c, []string{contentTopic})
-	}
-	time.Sleep(500 * time.Millisecond)
-
-	// Send a message to every node.
-	msgs := make([]*pb.WakuMessage, len(clients))
-	for i := range clients {
-		msgs[i] = test.NewMessage(contentTopic, int64(i+1), fmt.Sprintf("msg%d", i+1))
-	}
-	for i, sender := range clients {
-		test.Publish(t, sender, msgs[i])
-	}
-
-	// Expect them to be relayed to all nodes.
-	for _, envC := range envCs {
-		test.SubscribeExpect(t, envC, msgs)
-	}
-
-	// Expect that they've all been stored on each node.
-	for i, c := range clients {
-		expectQueryMessagesEventually(t, c, bootstrapAddrs[i], []string{contentTopic}, msgs)
-	}
-}
-
-func testName(s string) string {
-	return fmt.Sprintf("%s/%d", s, time.Now().UTC().Unix())
-}
-
-func fetchBootstrapAddrs(env string) ([]string, error) {
-	client := &http.Client{}
-	r, err := client.Get(envNodesURL)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Body.Close()
-
-	var manifest map[string]interface{}
-	err = json.NewDecoder(r.Body).Decode(&manifest)
-	if err != nil {
-		return nil, err
-	}
-
-	envManifest := manifest[env].(map[string]interface{})
-	addrs := make([]string, len(envManifest))
-	i := 0
-	for _, addr := range envManifest {
-		addrs[i] = addr.(string)
-		i++
-	}
-
-	return addrs, nil
-}
-
-func queryMessages(t *testing.T, c *wakunode.WakuNode, peerAddr string, contentTopics []string) []*pb.WakuMessage {
 	log, err := zap.NewDevelopment()
 	require.NoError(t, err)
-
-	pi, err := peer.AddrInfoFromString(peerAddr)
-	require.NoError(t, err)
-
-	client, err := store.NewClient(
-		store.WithClientLog(log),
-		store.WithClientHost(c.Host()),
-		store.WithClientPeer(pi.ID),
-	)
-	require.NoError(t, err)
-
-	msgs := []*pb.WakuMessage{}
-	ctx := context.Background()
-	contentFilters := make([]*pb.ContentFilter, len(contentTopics))
-	for i, contentTopic := range contentTopics {
-		contentFilters[i] = &pb.ContentFilter{
-			ContentTopic: contentTopic,
-		}
-	}
-	msgCount, err := client.Query(ctx, &pb.HistoryQuery{
-		PubsubTopic:    relay.DefaultWakuTopic,
-		ContentFilters: contentFilters,
-	}, func(res *pb.HistoryResponse) (int, bool) {
-		msgs = append(msgs, res.Messages...)
-		return len(res.Messages), true
+	e := New(ctx, log, &Config{
+		NetworkEnv: localNetworkEnv,
+		NodesURL:   localNodesURL,
 	})
+	err = e.Run()
 	require.NoError(t, err)
-	require.Equal(t, msgCount, len(msgs))
-
-	return msgs
-}
-
-func expectQueryMessagesEventually(t *testing.T, n *wakunode.WakuNode, peerAddr string, contentTopics []string, expectedMsgs []*pb.WakuMessage) []*pb.WakuMessage {
-	var msgs []*pb.WakuMessage
-	require.Eventually(t, func() bool {
-		msgs = queryMessages(t, n, peerAddr, contentTopics)
-		return len(msgs) == len(expectedMsgs)
-	}, 3*time.Second, 500*time.Millisecond)
-	require.ElementsMatch(t, expectedMsgs, msgs)
-	return msgs
 }
