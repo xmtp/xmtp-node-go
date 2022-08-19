c6bb74847f436b077f8cee8a9805f41f1fd39b99
Martin Kobetic <mkobetic@users.noreply.github.com>
2022-08-19 14:23:18 -0400

    Add standard GRPC metrics (#119)
    * Add standard GRPC metrics
    
    * fix
    
    * lint

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -1,15 +1,22 @@
 package metrics
 
 import (
+	"context"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/status-im/go-waku/waku/metrics"
 	v2metrics "github.com/status-im/go-waku/waku/v2/metrics"
+	"github.com/xmtp/xmtp-node-go/pkg/logging"
+	"github.com/xmtp/xmtp-node-go/pkg/tracing"
 	"go.opencensus.io/stats/view"
 	"go.uber.org/zap"
 )
 
 // Server wraps go-waku metrics server, so that we don't need to reference the go-waku package anywhere
 type Server struct {
-	*metrics.Server
+	waku *metrics.Server
+	http *http.Server
 }
 
 // re-export go-waku functions
@@ -19,7 +26,25 @@ var (
 )
 
 func NewMetricsServer(address string, port int, logger *zap.Logger) *Server {
-	return &Server{metrics.NewMetricsServer(address, port, logger)}
+	return &Server{waku: metrics.NewMetricsServer(address, port, logger)}
+}
+
+func (s *Server) Start(ctx context.Context) {
+	log := logging.From(ctx).Named("metrics")
+	go tracing.PanicWrap(ctx, "waku metrics server", func(_ context.Context) { s.waku.Start() })
+	s.http = &http.Server{Addr: ":8009", Handler: promhttp.Handler()}
+	go tracing.PanicWrap(ctx, "metrics server", func(_ context.Context) {
+		log.Info("server stopped", zap.Error(s.http.ListenAndServe()))
+	})
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	wErr := s.waku.Stop(ctx)
+	err := s.http.Shutdown(ctx)
+	if wErr != nil {
+		return wErr
+	}
+	return err
 }
 
 func RegisterViews(logger *zap.Logger) {
