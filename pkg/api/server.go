49dbec45953fda3af3ef899229249854f040f9ba
Steven Normore <snormore@gmail.com>
2022-10-27 13:23:45 -0400

    Allow CORS origin wildcard

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -215,12 +215,10 @@ func preflightHandler(w http.ResponseWriter, r *http.Request) {
 
 func allowCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if origin := r.Header.Get("Origin"); origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
-				preflightHandler(w, r)
-				return
-			}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+			preflightHandler(w, r)
+			return
 		}
 		h.ServeHTTP(w, r)
 	})
