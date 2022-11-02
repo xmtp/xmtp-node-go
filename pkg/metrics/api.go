f06659040e0b231da1fe203de0393d0254db1c7e
Martin Kobetic <mkobetic@users.noreply.github.com>
2022-11-02 13:04:24 -0400

    tweak

--- pkg/metrics/api.go
+++ pkg/metrics/api.go
@@ -2,6 +2,9 @@ package metrics
 
 import (
 	"context"
+	"strings"
+
+	proto "github.com/xmtp/proto/go/message_api/v1"
 
 	"github.com/xmtp/xmtp-node-go/pkg/logging"
 	"go.opencensus.io/stats"
@@ -34,6 +37,31 @@ var apiRequestsView = &view.View{
 	},
 }
 
+var topicCategoryTag, _ = tag.NewKey("topic-category")
+var publishedEnvelopeMeasure = stats.Int64("published_envelope", "size of a published envelope", stats.UnitBytes)
+var publishedEnvelopeView = &view.View{
+	Name:        "xmtp_published_envelope",
+	Measure:     publishedEnvelopeMeasure,
+	Description: "Size of a published envelope",
+	Aggregation: view.Distribution(100, 1000, 10000, 100000),
+	TagKeys:     []tag.Key{topicCategoryTag},
+}
+
+func EmitPublishedEnvelope(ctx context.Context, env *proto.Envelope) {
+	topicCategory := categoryFromTopic(env.ContentTopic)
+	size := int64(len(env.Message))
+	mutators := []tag.Mutator{tag.Insert(topicCategoryTag, topicCategory)}
+	err := recordWithTags(ctx, mutators, publishedEnvelopeMeasure.M(size))
+	if err != nil {
+		logging.From(ctx).Error("recording metric",
+			zap.Error(err),
+			zap.String("metric", publishedEnvelopeView.Name),
+			zap.Int64("size", size),
+			zap.String("topic_category", topicCategory),
+		)
+	}
+}
+
 func EmitAPIRequest(ctx context.Context, serviceName, methodName, clientName, clientVersion, appName, appVersion string) {
 	mutators := []tag.Mutator{
 		tag.Insert(clientNameTag, clientName),
@@ -55,4 +83,28 @@ func EmitAPIRequest(ctx context.Context, serviceName, methodName, clientName, cl
 			zap.String("app_version", appVersion),
 		)
 	}
+}
+
+func categoryFromPrefix(topicPrefix string) string {
+	for prefix, category := range map[string]string{
+		"contact":      "contact",
+		"intro":        "v1-intro",
+		"dm":           "v1-conversation",
+		"invite":       "v2-invite",
+		"m":            "v2-conversation",
+		"privatestore": "private",
+	} {
+		if topicPrefix == prefix {
+			return category
+		}
+	}
+	return "invalid"
+}
+
+func categoryFromTopic(topic string) string {
+	prefix, _, hasPrefix := strings.Cut(topic, "-")
+	if hasPrefix {
+		return categoryFromPrefix(prefix)
+	}
+	return "invalid"
 }
