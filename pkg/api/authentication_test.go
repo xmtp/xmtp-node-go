eeca9966d718ff4b442fff2342b3261626853994
Daniel McCartney <dmccartney@gmail.com>
2022-11-16 15:20:37 -0500

    fix: nil check auth data on token validation (#186)

--- pkg/api/authentication_test.go
+++ pkg/api/authentication_test.go
@@ -30,6 +30,15 @@ func Test_AuthnAllowedWithoutAuthn(t *testing.T) {
 	})
 }
 
+func Test_AuthnTokenMissingAuthData(t *testing.T) {
+	ctx := withMissingAuthData(t, context.Background())
+	testGRPCAndHTTP(t, ctx, func(t *testing.T, client messageclient.Client, server *Server) {
+		_, err := client.Publish(ctx, &messageV1.PublishRequest{})
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "missing auth data")
+	})
+}
+
 func Test_AuthnValidToken(t *testing.T) {
 	ctx := withAuth(t, context.Background())
 	testGRPCAndHTTP(t, ctx, func(t *testing.T, client messageclient.Client, server *Server) {
