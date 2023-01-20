8cb7c5634d609f8d8e309e4928bfac7bbe42c986
Steven Normore <snormore@gmail.com>
2023-01-20 10:29:34 -0500

    Use single batch delete query for cleaner

--- pkg/store/cleaner.go
+++ pkg/store/cleaner.go
@@ -1,8 +1,6 @@
 package store
 
 import (
-	"fmt"
-	"strings"
 	"time"
 
 	"go.uber.org/zap"
@@ -32,54 +30,31 @@ func (s *XmtpStore) cleanerLoop() {
 }
 
 func (s *XmtpStore) deleteNonXMTPMessagesBatch(log *zap.Logger) error {
-	stmt, err := s.readerDB.Prepare("SELECT ctid FROM message WHERE receivertimestamp < $1 AND contenttopic NOT LIKE '/xmtp/%' LIMIT 10000")
+	stmt, err := s.db.Prepare(`
+		WITH msg AS (
+			SELECT ctid
+			FROM message
+			WHERE receivertimestamp < $1 AND contenttopic NOT LIKE '/xmtp/%'
+			LIMIT 1000
+			FOR UPDATE SKIP LOCKED
+		)
+		DELETE FROM message WHERE ctid IN (TABLE msg);
+	`)
 	if err != nil {
 		return err
 	}
 	timestampThreshold := time.Now().UTC().Add(time.Duration(s.cleaner.RetentionDays) * -1 * 24 * time.Hour).UnixNano()
-	rows, err := stmt.Query(timestampThreshold)
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-
-	ids := []any{}
-	for rows.Next() {
-		var id string
-		err = rows.Scan(&id)
-		if err != nil {
-			return err
-		}
-		ids = append(ids, id)
-	}
-
-	err = rows.Err()
+	res, err := stmt.Exec(timestampThreshold)
 	if err != nil {
 		return err
 	}
 
-	if len(ids) == 0 {
-		return nil
-	}
-
-	placeholders := make([]string, len(ids))
-	for i := 0; i < len(ids); i++ {
-		placeholders[i] = fmt.Sprintf("$%d", i+1)
-	}
-	stmt, err = s.db.Prepare("DELETE FROM message WHERE ctid IN (" + strings.Join(placeholders, ",") + ")")
-	if err != nil {
-		return err
-	}
-	res, err := stmt.Exec(ids...)
-	if err != nil {
-		return err
-	}
 	count, err := res.RowsAffected()
 	if err != nil {
 		return err
 	}
 
-	log.Info("deleted non-xmtp messages", zap.Int64("deleted", count), zap.Int("ids", len(ids)))
+	log.Info("deleted non-xmtp messages", zap.Int64("deleted", count))
 
 	return nil
 }
