54fb6173862b53639cf9252ee43236984d9f3d36
Steven Normore <snormore@gmail.com>
2023-01-26 17:58:01 -0500

    Add count check in cleaner

--- pkg/store/cleaner.go
+++ pkg/store/cleaner.go
@@ -38,16 +38,41 @@ func (s *XmtpStore) cleanerLoop() {
 }
 
 func (s *XmtpStore) deleteNonXMTPMessagesBatch(log *zap.Logger) (int64, error) {
+	started := time.Now().UTC()
+	timestampThreshold := time.Now().UTC().Add(time.Duration(s.cleaner.RetentionDays) * -1 * 24 * time.Hour).UnixNano()
+	whereClause := "receivertimestamp < $1 AND should_expire IS TRUE"
+
+	stmt, err := s.cleanerDB.Prepare("SELECT COUNT(1) FROM message WHERE " + whereClause + ";")
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(timestampThreshold)
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+	if !rows.Next() {
+		return 0, nil
+	}
+	var count int64
+	err = rows.Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+	if count < int64(s.cleaner.BatchSize) {
+		return 0, nil
+	}
+
 	// We use a single atomic query here instead of breaking it up to hit the
 	// reader for the non-indexed NOT LIKE query first, because ctid can change
 	// during a full vacuum of the DB, so we want to avoid conflicting with
 	// that scenario and deleting the wrong data.
-	started := time.Now().UTC()
-	stmt, err := s.cleanerDB.Prepare(`
+	stmt, err = s.cleanerDB.Prepare(`
 		WITH msg AS (
 			SELECT ctid
 			FROM message
-			WHERE receivertimestamp < $1 AND should_expire IS TRUE
+			WHERE ` + whereClause + `
 			LIMIT $2
 			FOR UPDATE SKIP LOCKED
 		)
@@ -57,13 +82,11 @@ func (s *XmtpStore) deleteNonXMTPMessagesBatch(log *zap.Logger) (int64, error) {
 		return 0, err
 	}
 	defer stmt.Close()
-	timestampThreshold := time.Now().UTC().Add(time.Duration(s.cleaner.RetentionDays) * -1 * 24 * time.Hour).UnixNano()
 	res, err := stmt.Exec(timestampThreshold, s.cleaner.BatchSize)
 	if err != nil {
 		return 0, err
 	}
-
-	count, err := res.RowsAffected()
+	count, err = res.RowsAffected()
 	if err != nil {
 		return 0, err
 	}
