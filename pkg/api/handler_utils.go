f608c05ccf9ab63fa78ae58cf0a01fd57a354819
Michael Xu <michaelx@xmtp.com>
2023-01-27 13:47:09 -0800

    add discussion about why we only gzip for /query and /batch-query

--- pkg/api/handler_utils.go
+++ pkg/api/handler_utils.go
@@ -19,6 +19,8 @@ func (w gzipResponseWriter) Write(b []byte) (int, error) {
 func gzipWrapper(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Check that path ends in /query or /batch-query
+		// since clients still accept gzip encoding for /subscribe which is problematic
+		// some discussion: https://github.com/typelevel/fs2/issues/1831
 		if !strings.HasSuffix(r.URL.Path, "/query") && !strings.HasSuffix(r.URL.Path, "/batch-query") {
 			handler.ServeHTTP(w, r)
 			return
