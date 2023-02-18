464c0f7b6a8a1867bd9e23dbde6599c47a1c50f0
Steven Normore <snormore@gmail.com>
2023-02-18 11:57:15 -0500

    Fix store query tests to use tmp test DB

--- pkg/store/query_test.go
+++ pkg/store/query_test.go
@@ -1,42 +1,26 @@
 package store
 
 import (
-	"database/sql"
-	"os"
 	"testing"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3" // Blank import to register the sqlite3 driver
 	"github.com/stretchr/testify/require"
-	"github.com/uptrace/bun/driver/pgdriver"
 	messagev1 "github.com/xmtp/proto/v3/go/message_api/v1"
 	test "github.com/xmtp/xmtp-node-go/pkg/testing"
 )
 
 const TOPIC = "test"
 
-func NewMock() *sql.DB {
-	dsn, hasDsn := os.LookupEnv("MESSAGE_POSTGRES_CONNECTION_STRING")
-	if !hasDsn {
-		dsn = "postgres://postgres:xmtp@localhost:15432/postgres?sslmode=disable"
-	}
-	db := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
-
-	return db
-}
-
 func createAndFillDb(t *testing.T) (*Store, func(), []*messagev1.Envelope) {
-	db := NewMock()
 	store, cleanup := newTestStore(t)
-	_, err := db.Exec("TRUNCATE TABLE message;")
-	require.NoError(t, err)
 
 	ts := time.Now().UTC().UnixNano()
 	msg1 := test.NewEnvelope("test1", 1, "")
 	msg2 := test.NewEnvelope("test2", 2, "")
 	msg3 := test.NewEnvelope("test3", 3, "")
 
-	err = store.insertMessage(msg1, ts)
+	err := store.insertMessage(msg1, ts)
 	require.NoError(t, err)
 
 	err = store.insertMessage(msg2, ts)
@@ -45,10 +29,7 @@ func createAndFillDb(t *testing.T) (*Store, func(), []*messagev1.Envelope) {
 	err = store.insertMessage(msg3, ts)
 	require.NoError(t, err)
 
-	return store, func() {
-		cleanup()
-		db.Close()
-	}, []*messagev1.Envelope{msg1, msg2, msg3}
+	return store, cleanup, []*messagev1.Envelope{msg1, msg2, msg3}
 }
 
 func TestQuerySimple(t *testing.T) {
