987915d997f21abc09941cf0b914adf660b07464
Steven Normore <steven@xmtp.com>
2024-01-23 19:28:45 -0500

    Add error logging to subscribe2 (#345)
    * Add error logging to subscribe2
    
    * Add error subscribing log label for number of topics

--- pkg/api/message/v1/service.go
+++ pkg/api/message/v1/service.go
@@ -187,7 +187,7 @@ func (s *Service) Subscribe(req *proto.SubscribeRequest, stream proto.MessageApi
 			}()
 		})
 		if err != nil {
-			log.Error("error subscribing", zap.Error(err))
+			log.Error("error subscribing", zap.Error(err), zap.Int("topics", len(req.ContentTopics)))
 			return err
 		}
 		defer func() {
@@ -291,6 +291,7 @@ func (s *Service) Subscribe2(stream proto.MessageApi_Subscribe2Server) error {
 						}()
 					})
 					if err != nil {
+						log.Error("error subscribing", zap.Error(err), zap.Int("topics", len(req.ContentTopics)))
 						return err
 					}
 					subs[topic] = sub
