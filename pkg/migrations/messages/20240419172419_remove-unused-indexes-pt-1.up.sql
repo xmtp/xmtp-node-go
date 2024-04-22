6107c1e59bbe82b0a3181655d9754554173556bb
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-04-22 09:34:44 -0700

    Don't make it concurrent

--- pkg/migrations/messages/20240419172419_remove-unused-indexes-pt-1.up.sql
+++ pkg/migrations/messages/20240419172419_remove-unused-indexes-pt-1.up.sql
@@ -1,4 +1,5 @@
 SET statement_timeout = 0;
 
-DROP INDEX CONCURRENTLY IF EXISTS messageindex;
-DROP INDEX CONCURRENTLY IF EXISTS message_pubsubtopic_idx;
+-- bun:split
+
+DROP INDEX IF EXISTS message_pubsubtopic_idx;
