520f1c10a51b8fc5d06cb79bee6ebe1a79b7bdfd
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-04-25 16:37:25 -0700

    Define SQL formatting (#384)
    * Define SQL formatting
    
    * Define SQL formatting

--- pkg/migrations/mls/20231023050806_init-schema.up.sql
+++ pkg/migrations/mls/20231023050806_init-schema.up.sql
@@ -1,17 +1,15 @@
-SET
-    statement_timeout = 0;
+SET statement_timeout = 0;
 
 --bun:split
-CREATE TABLE installations (
-    id BYTEA PRIMARY KEY,
-    wallet_address TEXT NOT NULL,
-    created_at BIGINT NOT NULL,
-    updated_at BIGINT NOT NULL,
-    credential_identity BYTEA NOT NULL,
-    revoked_at BIGINT,
-
-    key_package BYTEA NOT NULL,
-    expiration BIGINT NOT NULL
+CREATE TABLE installations(
+	id BYTEA PRIMARY KEY,
+	wallet_address TEXT NOT NULL,
+	created_at BIGINT NOT NULL,
+	updated_at BIGINT NOT NULL,
+	credential_identity BYTEA NOT NULL,
+	revoked_at BIGINT,
+	key_package BYTEA NOT NULL,
+	expiration BIGINT NOT NULL
 );
 
 --bun:split
@@ -22,3 +20,4 @@ CREATE INDEX idx_installations_created_at ON installations(created_at);
 
 --bun:split
 CREATE INDEX idx_installations_revoked_at ON installations(revoked_at);
+
--- pkg/migrations/mls/20240109001927_add-messages.down.sql
+++ pkg/migrations/mls/20240109001927_add-messages.down.sql
@@ -1,6 +1,5 @@
 SET statement_timeout = 0;
 
 --bun:split
-
 DROP TABLE IF EXISTS messages;
 
