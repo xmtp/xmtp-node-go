e05d645dfc756c4529cc0ed2d5c3d3c64a8ef13d
Richard Hua <rich@rkhua.com>
2024-08-29 11:35:02 -0700

    Lock per-entity

--- pkg/migrations/mls/20240829001344_serial-ids.up.sql
+++ pkg/migrations/mls/20240829001344_serial-ids.up.sql
@@ -5,7 +5,7 @@ BEGIN
 	-- Ensures that the generated sequence ID matches the insertion order
 	-- Only released at the end of the enclosing transaction - beware if called within a long transaction
 	PERFORM
-		pg_advisory_xact_lock(hashtext('group_messages_sequence'));
+		pg_advisory_xact_lock(hashtext('group_messages_sequence'), hashtext(encode(group_id, 'hex')));
 	RETURN QUERY INSERT INTO group_messages(group_id, data, group_id_data_hash)
 		VALUES(group_id, data, group_id_data_hash)
 	RETURNING
@@ -19,7 +19,7 @@ CREATE FUNCTION insert_welcome_message(installation_key BYTEA, data BYTEA, insta
 	AS $$
 BEGIN
 	PERFORM
-		pg_advisory_xact_lock(hashtext('welcome_messages_sequence'));
+		pg_advisory_xact_lock(hashtext('welcome_messages_sequence'), hashtext(encode(installation_key, 'hex')));
 	RETURN QUERY INSERT INTO welcome_messages(installation_key, data, installation_key_data_hash, hpke_public_key)
 		VALUES(installation_key, data, installation_key_data_hash, hpke_public_key)
 	RETURNING
@@ -33,7 +33,7 @@ CREATE FUNCTION insert_inbox_log(inbox_id BYTEA, server_timestamp_ns BIGINT, ide
 	AS $$
 BEGIN
 	PERFORM
-		pg_advisory_xact_lock(hashtext('inbox_log_sequence'));
+		pg_advisory_xact_lock(hashtext('inbox_log_sequence'), hashtext(encode(inbox_id, 'hex')));
 	RETURN QUERY INSERT INTO inbox_log(inbox_id, server_timestamp_ns, identity_update_proto)
 		VALUES(inbox_id, server_timestamp_ns, identity_update_proto)
 	RETURNING
