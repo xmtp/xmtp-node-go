948ee588b8f8f691d54ca0fc88b1a64d03efd1e3
Richard Hua <rich@rkhua.com>
2025-04-30 22:18:30 -0700

    Stub out gating

--- pkg/api/gating.go
+++ pkg/api/gating.go
@@ -3,9 +3,14 @@ package api
 import (
 	"context"
 
+	apicontext "github.com/xmtp/xmtp-node-go/pkg/api/message/v1/context"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const IS_GATING_ENABLED = false
 
 type GatingInterceptor struct {
 	log *zap.Logger
@@ -24,6 +29,10 @@ func (ti *GatingInterceptor) Unary() grpc.UnaryServerInterceptor {
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler,
 	) (interface{}, error) {
+		ri := apicontext.NewRequesterInfo(ctx)
+		if IS_GATING_ENABLED && !ri.IsSupportedClient {
+			return nil, status.Errorf(codes.Unimplemented, "unsupported libxmtp version "+ri.LibxmtpVersion)
+		}
 		res, err := handler(ctx, req)
 		return res, err
 	}
@@ -36,6 +45,10 @@ func (ti *GatingInterceptor) Stream() grpc.StreamServerInterceptor {
 		info *grpc.StreamServerInfo,
 		handler grpc.StreamHandler,
 	) error {
+		ri := apicontext.NewRequesterInfo(stream.Context())
+		if IS_GATING_ENABLED && !ri.IsSupportedClient {
+			return status.Errorf(codes.Unimplemented, "unsupported libxmtp version "+ri.LibxmtpVersion)
+		}
 		err := handler(srv, stream)
 		return err
 	}
