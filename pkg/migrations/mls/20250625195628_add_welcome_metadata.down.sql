780a40ec4f2d070c44f1b12b18291e65e0e9e635
Dakota Brink <git@kota.is>
2025-06-25 17:10:20 -0400

    tweak down

--- pkg/migrations/mls/20250625195628_add_welcome_metadata.down.sql
+++ pkg/migrations/mls/20250625195628_add_welcome_metadata.down.sql
@@ -6,4 +6,8 @@ ALTER TABLE welcome_messages
 DROP COLUMN welcome_metadata;
 
 --bun:split
+ALTER TABLE welcome_messages
+ADD COLUMN message_cursor BIGINT NOT NULL DEFAULT 0;
+
+--bun:split
 DROP FUNCTION IF EXISTS insert_welcome_message_v4;
