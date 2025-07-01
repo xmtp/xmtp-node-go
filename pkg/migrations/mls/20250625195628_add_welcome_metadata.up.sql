c6c4e23d2f11f9681ce9b5ecdb786c4941dae1a9
Naomi Plasterer <naomi@xmtp.com>
2025-07-01 08:12:10 -0700

    Deploy main (#465)
    * Insert Welcome messages concurrently without a transaction (#430)
    
    * Revert "Revert "Extend Welcome to include Cursor"" (#462)
    
    * Revert "Revert "Extend Welcome to include Cursor" (#456)"
    
    This reverts commit 6f356b734d0a79b013454499aa60da3acf114b2c.
    
    * bump proto
    
    * migrations
    
    * migration update
    
    * sqlc
    
    * update queries
    
    * revert tools
    
    * cleanup
    
    * test fixes
    
    * tweak down
    
    * fix bad merge lint
    
    * try shell check
    
    * fix the bad merge
    
    * fix the bad merge
    
    ---------
    
    Co-authored-by: Naomi Plasterer <naomi@xmtp.com>
    
    ---------
    
    Co-authored-by: Nicholas Molnar <65710+neekolas@users.noreply.github.com>
    Co-authored-by: Dakota Brink <779390+codabrink@users.noreply.github.com>

---
+++ pkg/migrations/mls/20250625195628_add_welcome_metadata.up.sql
@@ -0,0 +1,32 @@
+SET
+    statement_timeout = 0;
+
+--bun:split
+ALTER TABLE welcome_messages
+DROP COLUMN message_cursor;
+
+--bun:split
+ALTER TABLE welcome_messages
+ADD COLUMN welcome_metadata BYTEA;
+
+--bun:split
+CREATE OR REPLACE FUNCTION insert_welcome_message_v4(
+        installation_key BYTEA,
+        data BYTEA,
+        installation_key_data_hash BYTEA,
+        hpke_public_key BYTEA,
+        wrapper_algorithm SMALLINT,
+        welcome_metadata BYTEA
+    )
+	RETURNS SETOF welcome_messages
+	AS $$
+BEGIN
+	PERFORM
+		pg_advisory_xact_lock(hashtext('welcome_messages_sequence'), hashtext(encode(installation_key, 'hex')));
+	RETURN QUERY INSERT INTO welcome_messages(installation_key, data, installation_key_data_hash, hpke_public_key, wrapper_algorithm, welcome_metadata)
+	VALUES(installation_key, data, installation_key_data_hash, hpke_public_key, wrapper_algorithm, welcome_metadata)
+	RETURNING
+		*;
+END;
+$$
+LANGUAGE plpgsql;
